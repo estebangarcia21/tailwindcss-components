d4dbc5585536b8d73f852083f6c9c1e8961f6b13
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-14 17:16:15 -0800

    Configured babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,36 @@
 const path = require("path")
 
 module.exports = {
-  entry: "src/index.ts",
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js"
   },
-  module: [
-    {
-      rules: /\.tsx?$/,
-      loader: "babel-loader"
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom"
     }
-  ]
+  }
 }
